1d24b52361fbf48aabbfa5a58bd617ed4c40ef6f
babers <baber777@yahoo.com>
2016-07-17 22:56:41 +0400

    First Version - July 17, 2016

---
+++ CableMarkersCalc/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace CableMarkersCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblZero = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.lblOne = new System.Windows.Forms.Label();
+            this.lblThree = new System.Windows.Forms.Label();
+            this.lblEight = new System.Windows.Forms.Label();
+            this.lblSeven = new System.Windows.Forms.Label();
+            this.lblSix = new System.Windows.Forms.Label();
+            this.lblFour = new System.Windows.Forms.Label();
+            this.lblNine = new System.Windows.Forms.Label();
+            this.lblFive = new System.Windows.Forms.Label();
+            this.lblTwo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(419, 83);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(100, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblZero
+            // 
+            this.lblZero.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblZero.Location = new System.Drawing.Point(12, 21);
+            this.lblZero.Name = "lblZero";
+            this.lblZero.Size = new System.Drawing.Size(92, 28);
+            this.lblZero.TabIndex = 1;
+            // 
+            // lblError
+            // 
+            this.lblError.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblError.Location = new System.Drawing.Point(12, 180);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(507, 19);
+            this.lblError.TabIndex = 2;
+            this.lblError.Text = "Message Box";
+            this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(419, 21);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 20);
+            this.txtStart.TabIndex = 3;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(419, 49);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 20);
+            this.txtEnd.TabIndex = 4;
+            // 
+            // lblOne
+            // 
+            this.lblOne.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblOne.Location = new System.Drawing.Point(12, 58);
+            this.lblOne.Name = "lblOne";
+            this.lblOne.Size = new System.Drawing.Size(92, 28);
+            this.lblOne.TabIndex = 5;
+            // 
+            // lblThree
+            // 
+            this.lblThree.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblThree.Location = new System.Drawing.Point(12, 130);
+            this.lblThree.Name = "lblThree";
+            this.lblThree.Size = new System.Drawing.Size(92, 28);
+            this.lblThree.TabIndex = 6;
+            // 
+            // lblEight
+            // 
+            this.lblEight.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblEight.Location = new System.Drawing.Point(234, 52);
+            this.lblEight.Name = "lblEight";
+            this.lblEight.Size = new System.Drawing.Size(92, 28);
+            this.lblEight.TabIndex = 7;
+          
+            // 
+            // lblSeven
+            // 
+            this.lblSeven.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSeven.Location = new System.Drawing.Point(234, 21);
+            this.lblSeven.Name = "lblSeven";
+            this.lblSeven.Size = new System.Drawing.Size(92, 28);
+            this.lblSeven.TabIndex = 8;
+            // 
+            // lblSix
+            // 
+            this.lblSix.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSix.Location = new System.Drawing.Point(120, 94);
+            this.lblSix.Name = "lblSix";
+            this.lblSix.Size = new System.Drawing.Size(92, 28);
+            this.lblSix.TabIndex = 9;
+            // 
+            // lblFour
+            // 
+            this.lblFour.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblFour.Location = new System.Drawing.Point(120, 21);
+            this.lblFour.Name = "lblFour";
+            this.lblFour.Size = new System.Drawing.Size(92, 28);
+            this.lblFour.TabIndex = 10;
+            // 
+            // lblNine
+            // 
+            this.lblNine.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblNine.Location = new System.Drawing.Point(234, 83);
+            this.lblNine.Name = "lblNine";
+            this.lblNine.Size = new System.Drawing.Size(92, 28);
+            this.lblNine.TabIndex = 11;
+            // 
+            // lblFive
+            // 
+            this.lblFive.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblFive.Location = new System.Drawing.Point(120, 58);
+            this.lblFive.Name = "lblFive";
+            this.lblFive.Size = new System.Drawing.Size(92, 28);
+            this.lblFive.TabIndex = 12;
+            // 
+            // lblTwo
+            // 
+            this.lblTwo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblTwo.Location = new System.Drawing.Point(12, 94);
+            this.lblTwo.Name = "lblTwo";
+            this.lblTwo.Size = new System.Drawing.Size(92, 28);
+            this.lblTwo.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(345, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Start Range";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "End Range";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(546, 218);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTwo);
+            this.Controls.Add(this.lblFive);
+            this.Controls.Add(this.lblNine);
+            this.Controls.Add(this.lblFour);
+            this.Controls.Add(this.lblSix);
+            this.Controls.Add(this.lblSeven);
+            this.Controls.Add(this.lblEight);
+            this.Controls.Add(this.lblThree);
+            this.Controls.Add(this.lblOne);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblZero);
+            this.Controls.Add(this.btnCalc);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblZero;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label lblOne;
+        private System.Windows.Forms.Label lblThree;
+        private System.Windows.Forms.Label lblEight;
+        private System.Windows.Forms.Label lblSeven;
+        private System.Windows.Forms.Label lblSix;
+        private System.Windows.Forms.Label lblFour;
+        private System.Windows.Forms.Label lblNine;
+        private System.Windows.Forms.Label lblFive;
+        private System.Windows.Forms.Label lblTwo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CableMarkersCalc/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CableMarkersCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            calculateCableMarkers();
+
+        }
+
+        public void resetControls()
+        {
+            lblError.Text = "";
+
+        }
+
+        public void calculateCableMarkers()
+        {
+            try
+            {
+                int intS = Convert.ToInt32(txtStart.Text);
+                int intL = Convert.ToInt32(txtEnd.Text);
+
+                int a0 = 0;
+                int a1 = 0;
+                int a2 = 0;
+                int a3 = 0;
+                int a4 = 0;
+                int a5 = 0;
+                int a6 = 0;
+                int a7 = 0;
+                int a8 = 0;
+                int a9 = 0;
+
+                if (intS <= intL)
+                {
+
+                    for (int i = intS; i <= intL; i++)
+                    {
+                        string str = i.ToString();
+
+
+                        foreach (Match m in Regex.Matches(str, "0"))
+                            a0++;
+                        foreach (Match m in Regex.Matches(str, "1"))
+                            a1++;
+                        foreach (Match m in Regex.Matches(str, "2"))
+                            a2++;
+                        foreach (Match m in Regex.Matches(str, "3"))
+                            a3++;
+                        foreach (Match m in Regex.Matches(str, "4"))
+                            a4++;
+                        foreach (Match m in Regex.Matches(str, "5"))
+                            a5++;
+                        foreach (Match m in Regex.Matches(str, "6"))
+                            a6++;
+                        foreach (Match m in Regex.Matches(str, "7"))
+                            a7++;
+                        foreach (Match m in Regex.Matches(str, "8"))
+                            a8++;
+                        foreach (Match m in Regex.Matches(str, "9"))
+                            a9++;
+
+                    }
+
+                    if (a0 != 0)
+                        lblZero.Text = "0s = " + a0;
+                    if (a1 != 0)
+                        lblOne.Text = "1s = " + a1;
+                    if (a2 != 0)
+                        lblTwo.Text = "2s = " + a2;
+                    if (a3 != 0)
+                        lblThree.Text = "3s = " + a3;
+                    if (a4 != 0)
+                        lblFour.Text = "4s = " + a4;
+                    if (a5 != 0)
+                        lblFive.Text = "5s = " + a5;
+                    if (a6 != 0)
+                        lblSix.Text = "6s = " + a6;
+                    if (a7 != 0)
+                        lblSeven.Text = "7s = " + a7;
+                    if (a8 != 0)
+                        lblEight.Text = "8s = " + a8;
+                    if (a9 != 0)
+                        lblNine.Text = "9s = " + a9;
+
+                   
+                }
+                else
+                {
+                    lblError.Text = "Start Integer must be lesser than End Integer";
+                }
+                
+            }
+            catch(Exception ex)
+            {
+               lblError.Text=  ex.Message.ToString();
+            }
+
+        }
+
+       
+    }
+}
